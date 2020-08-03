3c9eb47414d593a6242e5f22637c4686d0c737f9
Jukka Lehikoinen <57388263+JukkaLehikoinen@users.noreply.github.com>
2020-08-03 14:30:52 +0300

    Updates

---
+++ precache-manifest.0bfcdf4847e834a5abe8bdf93f2c34b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb817bafd97b44c5019d37e900a93e5",
+    "url": "/kiinteistohuolto-front/index.html"
+  },
+  {
+    "revision": "e35a8d5e737a82296d57",
+    "url": "/kiinteistohuolto-front/static/css/2.bfa46653.chunk.css"
+  },
+  {
+    "revision": "052393e3b934cbb4d7b8",
+    "url": "/kiinteistohuolto-front/static/css/main.282ddb70.chunk.css"
+  },
+  {
+    "revision": "e35a8d5e737a82296d57",
+    "url": "/kiinteistohuolto-front/static/js/2.0db251bf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/kiinteistohuolto-front/static/js/2.0db251bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052393e3b934cbb4d7b8",
+    "url": "/kiinteistohuolto-front/static/js/main.4a40c328.chunk.js"
+  },
+  {
+    "revision": "19e0a16b7f2ad7bcbe60",
+    "url": "/kiinteistohuolto-front/static/js/runtime-main.61baca62.js"
+  }
+]);
